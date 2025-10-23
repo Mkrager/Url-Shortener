ed0734aacbb05fa9eaacb0c563ad70df2b8c29a8
Smaga Max <smaga.max@gmail.com>
2025-10-24 00:36:30 +0300

    implement validation behavior

--- UrlShortener.Application/ApplicationServiceRegistration.cs
+++ UrlShortener.Application/ApplicationServiceRegistration.cs
@@ -1,6 +1,8 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using UrlShortener.Application.Behaviours;
 
 namespace UrlShortener.Application
 {
@@ -11,6 +13,10 @@ namespace UrlShortener.Application
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
             return services;
         }
